d7f74fbe108a76d3b83dda9620986cc8ebf2db3f
alinalex <alin.rauta@thebusinessclub.ro>
2016-02-13 02:54:40 +0200

    forth
    commit

---
+++ script.js
@@ -0,0 +1,72 @@
+$(document).ready(function (){
+            $("#link1").click(function (){
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: $("#content2").offset().top
+                }, 1250);
+            });
+
+            $("#link2").click(function (){
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: $("#content3").offset().top
+                }, 1250);
+            });
+
+            $("#link3").click(function (){
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: $("#content4").offset().top
+                }, 1250);
+            });
+
+            $("#link4").click(function (){
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: $("#content5").offset().top
+                }, 1250);
+            });
+
+            $("#link5").click(function (){
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: $("#content6").offset().top
+                }, 1250);
+            });
+
+            $("#link6").click(function (){
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: $("#content7").offset().top
+                }, 1250);
+            });
+
+            $("#link7").click(function (){
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: $("#content8").offset().top
+                }, 1250);
+            });
+
+            $("#link8").click(function (){
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: $("#content9").offset().top
+                }, 1250);
+            });
+
+            $("#link9").click(function (){
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: $("#content10").offset().top
+                }, 1250);
+            });
+
+            $("#link10").click(function (){
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: $("#content2").offset().top
+                }, 1250);
+            });
+
+        });
